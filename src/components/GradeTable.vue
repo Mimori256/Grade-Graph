fa9f7bdbd4dc1fb7ff8f8ba00aaa1c227054a956
Mimori256 <mimorimori256@gmail.com>
2023-09-10 14:29:37 +0900

    add some files

---
+++ src/components/GradeTable.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { store } from "@/store/index";
+
+export default defineComponent({
+  setup() {
+    return { store };
+  },
+});
+</script>
+<template>
+  <h1>成績一覧</h1>
+</template>
---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import Course from "@/components/types/Course";
+import Vuex from "vuex";
+
+export const store = new Vuex.Store({
+  state: {
+    courses: [] as Course[],
+    year: "all",
+  },
+  mutations: {
+    setCourses(state, courses: Course[]) {
+      state.courses = courses;
+    },
+    setYear(state, year: string) {
+      state.year = year;
+    },
+  },
+});
